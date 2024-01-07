3a5fdce3dcc550c1504f3ae6edca8ab56c451964
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-07 11:40:17 +0600

    Redux Middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,15 @@ import rootReducer from "./rootReducer";
 
 // create our first middleware
 const myLogger = (store) => (next) => (action) => {
-    
+    console.log(`Action: ${JSON.stringify(action)}`);
+    console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+    const upcomingState = [action].reduce(rootReducer, store.getState());
+    console.log(`UpcomingState: ${JSON.stringify(upcomingState)}`);
+
+
+    // pass action
+    return next(action);
 }
 const store = createStore(rootReducer, applyMiddleware(myLogger));
 
